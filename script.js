718b7cfa80a3e1f363aa91de2c1088d79a7bbd74
Ivan <ivanjelisavcic6@gmail.com>
2024-10-26 11:19:10 +0200

    Iks Oks igra

---
+++ script.js
@@ -0,0 +1,91 @@
+const board = document.getElementById('board');
+const cells = document.querySelectorAll('[data-cell]');
+const status = document.getElementById('status');
+const restartButton = document.getElementById('restartButton');
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+let xTurn = true;
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    xTurn = true;
+    cells.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    status.textContent = '';
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = xTurn ? X_CLASS : O_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    xTurn = !xTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if (xTurn) {
+        board.classList.add(X_CLASS);
+    } else {
+        board.classList.add(O_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function endGame(draw) {
+    if (draw) {
+        status.textContent = 'Nerešeno!';
+    } else {
+        status.textContent = `${xTurn ? 'X' : 'O'} Pobeđuje!`;
+    }
+    cells.forEach(cell => {
+        cell.removeEventListener('click', handleClick);
+    });
+}
